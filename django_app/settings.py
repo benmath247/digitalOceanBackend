02956c5f8bc6f023b796ac0bd30f161eb09df10e
Ben Math <benjamin.aaron.math@gmail.com>
2024-06-24 15:48:55 -0400

    trying to fix static files

--- django_app/settings.py
+++ django_app/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/staticfiles/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Uncomment if you have extra static files and a directory in your GitHub repo.
