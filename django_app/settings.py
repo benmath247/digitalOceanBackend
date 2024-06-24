a925b316e2463fa3755659b4f72e46a995583708
Ben Math <benjamin.aaron.math@gmail.com>
2024-06-24 15:52:46 -0400

    fix settings

--- django_app/settings.py
+++ django_app/settings.py
@@ -117,9 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = "/staticfiles/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Uncomment if you have extra static files and a directory in your GitHub repo.
-# If you don't have this directory and have this uncommented your build will fail
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+# Add these if not present
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# Ensure you have the right storage backend
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
